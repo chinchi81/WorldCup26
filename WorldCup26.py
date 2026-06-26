4bf109aef7e14fe02a34dc441f4aa9161a42932f
chinchi81 <33224214+chinchi81@users.noreply.github.com>
2026-06-26 22:14:09 +0100

    Update WorldCup26.py

--- WorldCup26.py
+++ WorldCup26.py
@@ -433,13 +433,13 @@ for tab, sheet in zip(tabs, tab_names):
                 #    """
                 #st.components.v1.html(iframeH1, height=240)
 
-                #iframeI1 = """
-                #    <iframe src="https://datawrapper.dwcdn.net/HDiZE/" 
-                #            width="100%" 
-                #            height="500" 
-                #            frameborder="0"></iframe>
-                #    """
-                #st.components.v1.html(iframeI1, height=240)
+                iframeI1 = """
+                    <iframe src="https://datawrapper.dwcdn.net/HDiZE/" 
+                            width="100%" 
+                            height="500" 
+                            frameborder="0"></iframe>
+                    """
+                st.components.v1.html(iframeI1, height=240)
                 
                 #iframeJ1 = """
                 #    <iframe src="https://datawrapper.dwcdn.net/X6ToB/" 
